92ec5afd0f3dc5e7b652d17375a4c322891d976f
Artyy <hasif_alp@hotmail.com>
2024-10-14 20:26:02 +0300

    lalalala
    lalalalala

--- Assets/Scripts/DayAndNightCircle.cs
+++ Assets/Scripts/DayAndNightCircle.cs
@@ -5,16 +5,15 @@ using UnityEngine.Rendering;
 
 public class DayAndNightCircle : MonoBehaviour
 {
+    [SerializeField] private Texture2D Sunrise;
     [SerializeField] private Texture2D Daylight;
     [SerializeField] private Texture2D Sunset;
     [SerializeField] private Texture2D Night;
-    [SerializeField] private Texture2D Sunrise;
-    [SerializeField] private Skybox SkyBox;
 
+    [SerializeField] private Gradient ColorSunrise;
     [SerializeField] private Gradient ColorDaylight;
     [SerializeField] private Gradient ColorSunset;
     [SerializeField] private Gradient ColorNight;
-    [SerializeField] private Gradient ColorSunrise;
     [SerializeField] private Light Light;
 
     public Transform DayNightCircle;
@@ -24,10 +23,11 @@ public class DayAndNightCircle : MonoBehaviour
     public int HoursMultiplier;
 
     private float Second;
+    private float AngleManager;
 
     private void Update()
     {
-        Second += Time.deltaTime * (1440 /  HoursMultiplier);
+        Second += Time.deltaTime * (1440 / HoursMultiplier);
         if (Second >= 60)
         {
             Minute += 1;
@@ -36,49 +36,55 @@ public class DayAndNightCircle : MonoBehaviour
             {
                 Hours += 1;
                 Minute = 0;
+                OnHoursChange(Hours);
                 if (Hours >= 24)
                 {
                     Hours = 0;
                 }
             }
         }
-        DayNightCircle.transform.eulerAngles = Quaternion.Euler(((Hours - 6) * 15f) + (Minute * 0.25f) + (0.0041666666666667f * Second), 0, 0).eulerAngles;
+        AngleManager = ((Hours - 6) * 15f) + (Minute * 0.25f) + (0.0041666666666667f * Second);
+        DayNightCircle.transform.eulerAngles = Quaternion.Euler(AngleManager, AngleManager / 4, 0).eulerAngles;
         //Debug.Log($"{Hours}, {Minute}, {Second} ---- {DayNightCircle.transform.eulerAngles.x}");
     }
 
-    /*private void OnHoursChange(int hours)
+    private void OnHoursChange(int hours)
     {
         if (hours == 6)
         {
-            StartCoroutine(SkyBoxLerp(NightToSunrise, SunriseToDaylight, 0));
-            StartCoroutine(SunColorLerp(ColorNightToSunrise, ColorSunriseToDaylight, 0));
+            StartCoroutine(SkyBoxLerp(Night, Sunrise, 10));
+            StartCoroutine(SunColorLerp(ColorSunrise, 10));
         }
         else if (hours == 8)
         {
-            StartCoroutine(SkyBoxLerp(SunriseToDaylight, DaylightToSunset, 0));
-            StartCoroutine(SunColorLerp(ColorSunriseToDaylight, ColorDaylightToSunset, 0));
+            StartCoroutine(SkyBoxLerp(Sunrise, Daylight, 10));
+            StartCoroutine(SunColorLerp(ColorDaylight, 10));
         }
         else if (hours == 18)
         {
-            StartCoroutine(SkyBoxLerp(DaylightToSunset, SunsetToNight, 0));
-            StartCoroutine(SunColorLerp(ColorDaylightToSunset, ColorSunsetToNight, 0));
+            StartCoroutine(SkyBoxLerp(Daylight, Sunset, 10));
+            StartCoroutine(SunColorLerp(ColorSunset, 10));
 
         }
         else if (hours == 22)
         {
-            StartCoroutine(SkyBoxLerp(SunsetToNight, NightToSunrise, 0));
-            StartCoroutine(SunColorLerp(ColorSunsetToNight, ColorNightToSunrise, 0));
+            StartCoroutine(SkyBoxLerp(Sunset, Night, 10));
+            StartCoroutine(SunColorLerp(ColorNight, 10));
         }
     }
 
     private IEnumerator SkyBoxLerp(Texture2D a, Texture2D b, float time)
     {
-        
-        yield return new WaitForSeconds(time);
-    }*/
 
-    private IEnumerator SunColorLerp(Gradient a, Gradient b, float time)
+        yield return null;
+    }
+
+    private IEnumerator SunColorLerp(Gradient LightGradient, float time)
     {
-        yield return new WaitForSeconds(time);
+        for (float i = 0; i < time; i += Time.deltaTime)
+        {
+            Light.color = LightGradient.Evaluate(i / time);
+            yield return null;
+        }
     }
 }
